d65a1d9ee480764a1cbd54effd897fe553dd1a9d
0x0001F36D <viyrex.aka.yuyu@gmail.com>
2020-02-13 00:25:44 +0800

    add demo image

--- Readme.md
+++ Readme.md
@@ -1,2 +1,4 @@
 # Flagship
 Most powerful flags value calculator and code stub generator written in C#.
+
+![img](./img/demo.png)
---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Flagship
+{
+    using System;
+    using System.Diagnostics;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Reflection;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encoded = Flags.Encode
+            (
+                ("a", Enumerate.Create<FieldAttributes>().All),
+                ("b", DayOfWeek.Monday),
+                ("c", MemberTypes.Method),
+                ("d", CallingConventions.Standard),
+                ("e", BindingFlags.DoNotWrapExceptions | BindingFlags.Public)
+            );
+
+            var stub = encoded.GenerateDecodeStub(isDebug: true);
+            Console.WriteLine(stub);
+        }
+    }
+}
